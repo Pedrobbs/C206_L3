4d1115bff3e157b0221194c8d7b241cff0242352
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-04-18 20:30:29 -0300

    Delete Aula6 directory

--- Aula6/src/Atleta.java
+++
@@ -1,4 +0,0 @@
-public abstract class Atleta {
-
-    public abstract void treinar();
-}
--- Aula6/src/Nadador.java
+++
@@ -1,6 +0,0 @@
-public class Nadador extends Atleta{
-    @Override
-    public void treinar() {
-        System.out.println("Treinando natação...");
-    }
-}
