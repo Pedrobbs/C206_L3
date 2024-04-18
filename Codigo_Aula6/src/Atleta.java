babf5325a2efa958fbccbd6a8dffe9f1fa9827f4
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-04-18 20:32:30 -0300

    Codigo_Aula6

---
+++ Codigo_Aula6/src/Atleta.java
@@ -0,0 +1,4 @@
+public abstract class Atleta {
+
+    public abstract void treinar();
+}
---
+++ Codigo_Aula6/src/Nadador.java
@@ -0,0 +1,6 @@
+public class Nadador extends Atleta{
+    @Override
+    public void treinar() {
+        System.out.println("Treinando natação...");
+    }
+}
