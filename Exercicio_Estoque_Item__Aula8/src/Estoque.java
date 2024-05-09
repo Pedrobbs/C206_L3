401cb283f33b4ad0fafa8002df7d1ff1e00754b1
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-05-09 09:31:46 -0300

    Códigos_Aula8

---
+++ Exercicio_Estoque_Item__Aula8/src/Estoque.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Estoque {
+    private ArrayList<Item> itensArray = new ArrayList<>();
+    private HashMap<String, Item> itensMap = new HashMap<>();
+
+    public void adcionarArray(Item item) {
+        itensArray.add(item);
+    }
+
+    public Item buscar(int posicao) {
+        return itensArray.get(posicao);
+    }
+
+    public void deletar(int posicao) {
+        itensArray.remove(posicao);
+    }
+
+    public void deletar(Item item) {
+        itensArray.remove(item);
+    }
+
+    public void modificar(Item item, int posicao) {
+        itensArray.set(posicao, item);
+    }
+
+    public ArrayList<Item> getItensArray() {
+        return itensArray;
+    }
+
+    public HashMap<String, Item> getItensMap() {
+        return itensMap;
+    }
+
+    public void mostrarItens() {
+        System.out.print("[");
+        for (Item item : itensArray) {
+            try {
+                System.out.print("  " + item.getNome() + " - " + item.getValor() + ", ");
+            } catch (NullPointerException e) {
+                System.out.println("Error: " + e);
+            }
+        }
+        System.out.println("]");
+    }
+
+    public void mostrarItensMap() {
+        System.out.print("[");
+        itensMap.forEach(
+                (k, v) -> {
+                    if (k != null) {
+                        System.out.print("  Chave : \"" + k + "\" Valor : {" + "  " + v.getNome() + " - " + v.getValor() + " }, ");
+                    }
+                }
+        );
+        System.out.println("]");
+    }
+}
